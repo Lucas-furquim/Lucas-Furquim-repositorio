f96ebda536fd15b15899b6da27d3a55b3b173e2c
Lucas Furquim <lucasviniciusfurquim@gmail.com>
2023-03-01 22:02:43 -0300

    habildiade

--- js/modulos/Habilidade.js
+++ js/modulos/Habilidade.js
@@ -4,27 +4,35 @@ export default function Habilidade() {
   const hab = [
     {
       nome: "Javascript",
+      imagem: "img/js-vector.svg",
     },
     {
       nome: "HTML5",
+      imagem: "img/html-vector.svg",
     },
     {
       nome: "CSS3",
+      imagem: "img/css-vector.svg",
     },
     {
       nome: "React.js",
+      imagem: "img/react-Vector.png",
     },
     {
       nome: "Wordpress",
+      imagem: "img/wp-vector.svg",
     },
     {
       nome: "Ui Desing",
+      imagem: "img/ui-vector.svg",
     },
     {
       nome: "Figma",
+      imagem: "img/figma-vector.svg",
     },
     {
       nome: "PHP",
+      imagem: "img/php-vector.svg",
     },
   ];
 
@@ -33,8 +41,13 @@ export default function Habilidade() {
   function addElemento() {
     hab.map((item) => {
       const criaP = document.createElement("p");
+      const criaLi = document.createElement("li");
+      const criaImg = document.createElement("img");
+      criaImg.src = item.imagem;
+      criaLi.appendChild(criaImg);
+      criaLi.appendChild(criaP);
       criaP.innerText = item.nome;
-      return elementos.push(criaP);
+      return elementos.push(criaLi);
     });
   }
 
