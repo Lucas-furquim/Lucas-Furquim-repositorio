742d1c198afca59c218455f0e61039d966aadaac
Lucas Furquim <lucasviniciusfurquim@gmail.com>
2023-03-07 20:54:33 -0300

    arrumando habilidades e navButton

--- js/modulos/btnHome.js
+++ js/modulos/btnHome.js
@@ -1,14 +1,26 @@
 export default class BtnHome {
-  constructor(btn, ativar, cabecalho) {
+  constructor(btn, ativar, cabecalho, navBtn) {
     this.cabecalho = document.querySelector(cabecalho);
     this.btn = document.querySelector(btn);
     this.home = document.querySelector(ativar);
+    this.navBtn = document.querySelector(navBtn);
   }
 
   addEvent() {
     this.btn.addEventListener("click", () => {
       this.home.classList.toggle("ativo");
       this.cabecalho.classList.toggle("ativo");
+      const verAtivo = this.home.classList.contains("ativo");
+      if (verAtivo) {
+        this.VerElemento();
+      }
+    });
+  }
+
+  VerElemento() {
+    this.navBtn.addEventListener("click", () => {
+      this.home.classList.remove("ativo");
+      this.cabecalho.classList.remove("ativo");
     });
   }
 
--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 import BtnHome from "./modulos/btnHome.js";
 import Habilidade from "./modulos/Habilidade.js";
 
-const btnHome = new BtnHome(".header-nav button", ".btnHome ", ".header");
+const btnHome = new BtnHome(
+  ".header-nav button",
+  ".btnHome ",
+  ".header",
+  ".btnHome-Nav ul"
+);
 btnHome.init();
 
 Habilidade();
