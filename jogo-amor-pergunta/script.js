f099d819545991c58888d2669fc9a32e67604d7c
Furquim~ <lucasviniu897@gmail.com>
2023-05-04 21:14:00 -0300

    casamento amor

---
+++ jogo-amor-pergunta/script.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector(".perigo");
+
+function taTonta(e) {
+  e.preventDefault();
+
+  e.target.style.top = Math.floor(Math.random() * 300) + "px";
+  e.target.style.left = Math.floor(Math.random() * 200) + "px";
+}
+
+btn.addEventListener("click", taTonta);
