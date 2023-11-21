4ed99adf31acab4b846067fcac78074cb91a6250
Furquim~ <lucasviniu897@gmail.com>
2023-11-21 20:20:42 -0300

    header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ["index.html"],
   theme: {},
-  plugins: [require("prettier-plugin-tailwindcss")],
 };
