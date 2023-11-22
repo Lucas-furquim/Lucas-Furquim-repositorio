cce4fdd84108d1c62b2f03edfdf7069a00806387
Furquim~ <lucasviniu897@gmail.com>
2023-11-22 19:33:33 -0300

    começando cabeçalho

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["index.html"],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        pricipal: "#01030E",
+        secundario: "#0F6BF5",
+        terceiro: "#332D9C",
+        quarto: "#332B96",
+        quinto: "#9A9A9A",
+      },
+    },
+  },
 };
