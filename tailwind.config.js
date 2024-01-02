c5e8ae3866721414aff1a1a21094aa8ed7b3b073
Furquim~ <lucasviniu897@gmail.com>
2024-01-02 11:38:31 -0300

    mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,21 @@ module.exports = {
         arial: ["Arial", "sans-serif"],
         inter: ["Inter"],
       },
+      screens: {
+        mxl: { max: "1279px" },
+        // => @media (max-width: 1279px) { ... }
+
+        mlg: { max: "1023px" },
+        // => @media (max-width: 1023px) { ... }
+
+        mmd: { max: "767px" },
+        // => @media (max-width: 767px) { ... }
+
+        msm: { max: "639px" },
+        // => @media (max-width: 639px) { ... }
+
+        mxs: { max: "439px" },
+      },
     },
   },
 };
